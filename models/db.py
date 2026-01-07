7dde3dea350d4a64476a1129543febe079657a82
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2026-01-08 01:28:52 +0530

    f it we ball

---
+++ models/db.py
@@ -0,0 +1,33 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+try:
+    connection = psycopg2.connect(
+        user=USER, password=PASSWORD, host=HOST, port=PORT, dbname=DBNAME
+    )
+    print("Connection successful!")
+
+    cursor = connection.cursor()
+
+    # Example query
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    print("Current Time:", result)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
+
+except Exception as e:
+    print(f"Failed to connect: {e}")
